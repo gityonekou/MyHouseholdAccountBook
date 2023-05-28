b05854e74a13b10104b323942a7b788b6173bda1
user <user@OWNER>
2023-05-28 20:35:01 +0900

    プロジェクト作成：初期コミット(大枠のみ)

---
+++ src/main/java/com/yonetani/webapp/accountbook/MyHouseholdAccountBookApplication.java
@@ -0,0 +1,18 @@
+package com.yonetani.webapp.accountbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * マイ家計簿用SpringBootプロジェクトのmainです
+ * @author user
+ *
+ */
+@SpringBootApplication
+public class MyHouseholdAccountBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHouseholdAccountBookApplication.class, args);
+	}
+
+}
